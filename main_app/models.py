c5ab1df222029e48d18efc54f75fad531a85f61f
zmewa079 <zannatul.mewa07@gmail.com>
2022-01-03 10:31:21 -0500

    add Book model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+  title = models.CharField(max_length=100)
+  author = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
