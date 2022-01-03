ae67a901a15dec058e3061cd142da08fe8148eb8
zmewa079 <zannatul.mewa07@gmail.com>
2022-01-03 01:38:18 -0500

    display book cards on all books page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  path('cats/', views.books_index, name='books_index'),
+  path('books/', views.books_index, name='books_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -7,9 +7,16 @@ class Book:
     self.author = author
     self.description = description
 
+books = [
+  Book('The Kite Runner', 'Khaled Hoesseini', 'A story about two boys and how a certain event during their childhood shape the different men they grow up to be.'),
+  Book('Pride and Prejudice', 'Jane Austen', 'novel')
+]
 
 def home(request):
   return HttpResponse('<h1>Welcome to The Book Maze</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def books_index(request):
+  return render(request, 'books/index.html', { 'books': books })
