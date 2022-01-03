9a30afc58bc024e488f420245905e64b51a631a8
zmewa079 <zannatul.mewa07@gmail.com>
2022-01-03 16:05:24 -0500

    add photo model

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+
 # Create your models here.
 class Book(models.Model):
   title = models.CharField(max_length=100)
   author = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
+  user = models.ForeignKey
 
 def __str__(self):
     return self.name
@@ -13,3 +15,10 @@ def __str__(self):
   # Add this method
 def get_absolute_url(self):
   return reverse('books_detail', kwargs={'book_id': self.id})
+
+class Photo(models.Model):
+  url = models.CharField(max_length=250)
+  book = models.OneToOneField(Book, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"Photo for book_id: {self.book_id} @{self.url}"
