2e775df545d84034dc3762cc5d92c0e735bbbf2f
zmewa079 <zannatul.mewa07@gmail.com>
2022-01-03 00:35:33 -0500

    add book model

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+class Book: 
+  def __init__(self, title, author, description):
+    self.title = title
+    self.author = author
+    self.description = description
+
 
 def home(request):
   return HttpResponse('<h1>Welcome to The Book Maze</h1>')
