133a413c35a0334bb7a6ecc313a5e7bd9394b2cf
zmewa079 <zannatul.mewa07@gmail.com>
2022-01-04 20:28:56 -0500

    Config deployment

--- thebookmaze/settings.py
+++ thebookmaze/settings.py
@@ -79,19 +79,19 @@ WSGI_APPLICATION = 'thebookmaze.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-# 	'default': {
-# 		'ENGINE': 'django.db.backends.postgresql',
-# 		'NAME': 'thebookmaze',
-# 	}
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': 'thebookmaze',
+	}
+}
 
 
 # Password validation
